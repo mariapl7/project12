b6bcf383b5521cd605b54de098e267b0a8fecfd7
miplo <miplotnova@mail.ru>
2025-04-26 22:27:14 +0500

    Homework_34_2

--- habit_tracker/habit_tracker/settings.py
+++ habit_tracker/habit_tracker/settings.py
@@ -66,10 +66,18 @@ TEMPLATES = [
 WSGI_APPLICATION = 'habit_tracker.wsgi.application'
 
 
+# if SERVER_TYPE == "local":
+#     host_db = "127.0.0.1"
+# else:
+#     host_db = "db"
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": os.getenv("POSTGRES_DB"),
+        "USER": os.getenv("POSTGRES_USER"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
+        "PORT": os.getenv("POSTGRES_PORT"),
     }
 }
 
