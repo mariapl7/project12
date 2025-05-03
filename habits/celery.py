f2e8d06e7c46edd5ff39baef7e709b10637ada72
miplo <miplotnova@mail.ru>
2025-05-03 11:40:46 +0500

    Homework_35_2

--- habits/celery.py
+++ habits/celery.py
@@ -1,5 +1,5 @@
 import os
-from celery import Celery
+from habits.celery import Celery
 
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'habit_tracker.settings')
--- habits/serializers.py
+++ habits/serializers.py
@@ -1,15 +1,20 @@
-from rest_framework import serializers
-from .models import Habit, HabitLog
+import os
+import sys
 
 
-class HabitSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Habit
-        fields = '__all__'
-        read_only_fields = ['user']  # Необходимо для защиты данных от редактирования пользователями
+def main():
+    """Run administrative task."""
+    os.environ.setdefault('DJANGO_SETTINGS_MODEL', 'habit_tracker.settings')
+    try:
+        from django.core.management import execute_from_command_line
+    except ImportError as exc:
+        raise ImportError(
+            "Couldn't import Django. Are you sure it's installed and "
+            "available on your PYTHONPATH environment variable? Did you "
+            "forget to activate a virtual environment?"
+        ) from exc
+    execute_from_command_line(sys.argv)
 
 
-class HabitLogSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = HabitLog
-        fields = '__all__'
+if __name__ == '__main__':
+    main()
